72c973711ffe49b0c198ad33348c99b652114e1e
Edvaldo Silva <edvaldodeveloper@gmail.com>
2021-01-29 16:43:42 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ace216bec67061d06ed1f258115f92f0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0ec01e36-f734-4e0a-b2cf-d8d315b371a8'
+};
